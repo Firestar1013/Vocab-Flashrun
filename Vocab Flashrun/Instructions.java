998ed2c7720705854df9268279bb49224b07e5b4
Ian <gnbethany@hotmail.com>
2016-01-09 08:40:16 -0800

    Initial Commit

---
+++ Vocab Flashrun/Instructions.java
@@ -0,0 +1,27 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Instructions here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Instructions extends World
+{
+
+    /**
+     * Constructor for objects of class Instructions.
+     * 
+     */
+    public Instructions()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(960, 720, 1); 
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("Backspace"))
+        Greenfoot.setWorld(new Title());
+    }
+}
---
+++ Vocab Flashrun/Title.java
@@ -0,0 +1,28 @@
+import greenfoot.*;
+import java.awt.Color;
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends World
+{
+
+    /**
+     * Constructor for objects of class Title.
+     * 
+     */
+    public Title()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(960, 720, 1); 
+       getBackground().fillRect(70,155,125,125);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("enter"))
+        Greenfoot.setWorld(new Instructions());
+    }
+}
